4095d01613538fc36a8cf4aef88078ab82e2fdfe
Acriid <wilrichgerdus@gmail.com>
2025-10-21 11:54:55 +0200

    Added PlayerItemSO Skeleton

---
+++ CottonCandy's Mansion/Assets/Scripts/ScriptableObjects/PlayerItemSO.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public class PlayerItemSO : ScriptableObject
+{
+    public String ItemName;
+    public String ItemDescription;
+    public Sprite ItemSprite;
+    public int ItemCost;
+}
