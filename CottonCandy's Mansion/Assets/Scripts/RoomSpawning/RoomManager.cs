8bd18fd0bac66bccb616158ad05267a6fa3f5dfd
Acriid <wilrichgerdus@gmail.com>
2025-11-08 20:32:40 +0200

    The rooms spawn wrong

--- CottonCandy's Mansion/Assets/Scripts/RoomSpawning/RoomManager.cs
+++ CottonCandy's Mansion/Assets/Scripts/RoomSpawning/RoomManager.cs
@@ -8,16 +8,22 @@ using System.Threading.Tasks;
 public class RoomManager : MonoBehaviour
 {
     public static RoomManager instance;
-    public string LoadLable = "Rooms";
-    private Dictionary<RoomSO.RoomType, List<RoomSO>> itemDictionary = new Dictionary<RoomSO.RoomType, List<RoomSO>>();
+    public string LoadLable = "Room";
+    private Dictionary<RoomSO.RoomType, List<RoomSO>> RoomDictionary = new Dictionary<RoomSO.RoomType, List<RoomSO>>();
     private List<RoomSO> rooms = new List<RoomSO>();
-    private List<RoomSO> CurrentLevel = new List<RoomSO>();
+    private List<GameObject> CurrentLevel = new List<GameObject>();
+    public Transform roomParent;
     async void Awake()
     {
         if (instance == null) { instance = this; }
         else { return; }
 
         await LoadAllRooms();
+
+        for(int i = 0; i < 3; i ++)
+        {
+            SpawnLevel();
+        }
     }
 
     private async Task LoadAllRooms()
@@ -29,6 +35,7 @@ public class RoomManager : MonoBehaviour
         if (handle.Status == AsyncOperationStatus.Succeeded)
         {
             rooms.AddRange(handle.Result);
+            Debug.Log("Loaded Rooms.");
         }
         else
         {
@@ -38,39 +45,66 @@ public class RoomManager : MonoBehaviour
         Addressables.Release(handle);
         foreach (RoomSO.RoomType roomType in Enum.GetValues(typeof(RoomSO.RoomType)))
         {
-            itemDictionary.Add(roomType, new List<RoomSO>());
+            RoomDictionary.Add(roomType, new List<RoomSO>());
         }
         foreach (RoomSO room in rooms)
         {
-            itemDictionary[room.Type].Add(room);
+            RoomDictionary[room.Type].Add(room);
         }
-
     }
-
+    #region SpawnRoom
+    private void SpawnLevel()
+    {
+        SpawnRoom(GetRandomRoom());
+    }
     private void SpawnRoom(RoomSO roomToSpawn)
     {
+        GameObject LevelObject;
         if (CurrentLevel.Count == 0)
         {
-
+            LevelObject = Instantiate(roomToSpawn.MainRoom, Vector3.zero, Quaternion.identity, roomParent);
+            CurrentLevel.Add(LevelObject);
+            return;
         }
+
+        GameObject CurrentRoom = CurrentLevel[CurrentLevel.Count - 1];
+        Debug.Log(CurrentRoom);
+        Vector3 RoomPosition = GetNewRoomPosition(roomToSpawn,CurrentRoom);
+        Quaternion RoomRotation = GetRoomRotation(CurrentRoom);
+        LevelObject = Instantiate(roomToSpawn.MainRoom, RoomPosition, RoomRotation, roomParent);
+        CurrentLevel.Add(LevelObject);
+
     }
 
-    private Vector3 GetNewRoomPosition(RoomSO roomToSpawn)
+    private Vector3 GetNewRoomPosition(RoomSO roomToSpawn,GameObject CurrentRoom)
     {
-        RoomSO CurrentRoom = CurrentLevel[CurrentLevel.Count - 1];
-        Transform spawnDoor = CurrentRoom.MainRoom.transform.Find("Doors/Door_Next");
+        //Gets room and door
+        Transform spawnDoor = CurrentRoom.transform.Find("Structure/Doors/Door_Next");
+        //Mirrors forward
         Vector3 newForward = spawnDoor.forward * -1;
-        Vector3 newSpawn = Vector3.Scale(spawnDoor.position, newForward) + newForward / 2f + Vector3.Scale(roomToSpawn.RoomSize / 2f, newForward);
+        //Gets new spawn
+        Vector3 newDoorPosition = Vector3.Scale(Vector3.Scale(spawnDoor.position, newForward),newForward);
+        Vector3 newSpawn = newDoorPosition + newForward / 2f + Vector3.Scale(roomToSpawn.RoomSize / 2f, newForward);        
         return newSpawn;
-    }
-    private Vector3 CorrectRotation(RoomSO roomToSpawn)
-    {
         
-        return Vector3.zero;
     }
+    private Quaternion GetRoomRotation(GameObject CurrentRoom)
+    {
+        //Gets room and door
+        Transform spawnDoor = CurrentRoom.transform.Find("Structure/Doors/Door_Next");
+        //Mirrors forward
+        Vector3 newForward = spawnDoor.forward * -1;
+        //Gets new Rotation
+        Quaternion result = Quaternion.LookRotation(newForward);
+        return result;
+    }
+    #endregion
     private RoomSO GetRandomRoom()
     {
-        return null;
+        int UpperBoundry = RoomDictionary[RoomSO.RoomType.Hallway].Count;
+        int RandomRoom = UnityEngine.Random.Range(0, UpperBoundry);
+        RoomSO result = RoomDictionary[RoomSO.RoomType.Hallway][RandomRoom];
+        return result;
     }
 }
 
