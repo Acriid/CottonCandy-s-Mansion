810e558cdb05775fb42c524b72ab3459bee7def1
Acriid <wilrichgerdus@gmail.com>
2025-12-17 22:20:16 +0200

    Added start of pickupmechanic

---
+++ CottonCandy's Mansion/Assets/Scripts/Player/PickUpMechanic.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpMechanic : MonoBehaviour
+{
+    [Header("Detection")]
+    [SerializeField] private float _detectionRadius = 3f;
+    [SerializeField] private LayerMask _itemLayer;
+
+
+    private List<Item> _itemsInRange = new();
+    private Item _targetItem;
+    private SphereCollider _detectionTrigger;
+
+    private void UpdateTargetItem(Transform player)
+    {
+        _itemsInRange.RemoveAll(item => item == null);
+
+        _targetItem = null;
+
+        if(_itemsInRange.Count == 0){return;}
+
+        float bestScore = float.MaxValue;
+
+        foreach(Item item in _itemsInRange)
+        {
+            float distance = Vector3.Distance(player.position,item.transform.position);
+
+            if(distance < bestScore)
+            {
+                bestScore = distance;
+                _targetItem = item;
+            }
+        }
+        
+    }
+
+    public void PickUpItem()
+    {
+        _itemsInRange.Remove(_targetItem);
+        _targetItem = null;
+        //TODO: in parent script
+        // Add item to inventory
+        // Show item in hotbar
+        // Show where character holds the item.
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (((1 << other.gameObject.layer) & _itemLayer) == 0) 
+            return;
+
+        Item item = other.GetComponent<Item>();
+
+        if(item != null && !_itemsInRange.Contains(item)) 
+            _itemsInRange.Add(item);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.TryGetComponent<Item>(out var item))
+        {
+            _itemsInRange.Remove(item);
+            if(_targetItem == item)
+                _targetItem = null;
+        }
+    }
+
+}
