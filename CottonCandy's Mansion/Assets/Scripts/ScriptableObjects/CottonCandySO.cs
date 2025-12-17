0b4e5c45400a8be1a8782c9ef2bda3579f968faa
Acriid <wilrichgerdus@gmail.com>
2025-12-17 10:36:23 +0200

    Start of CottonCandy

--- CottonCandy's Mansion/Assets/Scripts/ScriptableObjects/CottonCandySO.cs
+++ CottonCandy's Mansion/Assets/Scripts/ScriptableObjects/CottonCandySO.cs
@@ -1,11 +1,18 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "CottonCandy", menuName = "ScriptableObjects/CottonCandySO")]
 public class CottonCandySO : ScriptableObject
 {
-    [Header("Rarity Chances")]
-    public float CommonChance;
-    public float RareChance;
-    public float LegendaryChance;
+    //Gives deals which have an upside and downside (options in deals are scriptable objects where there are numbers and types. Upside and 10)
+    //Type of deal (bad,not that bad, good,pretty good,holy shit) are based on if you have more upsides than downsides. (something like a number)
+    //Changes chances based on the actions done during your playthrough
+    //Changes can range from giving better/worse deals changing the chances of specific items appearing.
+
+    //List of all deals yes.
+    //List of current upsides
+    //List of current downsides
+
+    [SerializeField] private List<OfferSO> _allDeals;
 }
---
+++ CottonCandy's Mansion/Assets/Scripts/ScriptableObjects/OfferSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CottonCandy", menuName = "ScriptableObjects/CottonCandySO")]
+public class OfferSO : ScriptableObject
+{
+
+}
